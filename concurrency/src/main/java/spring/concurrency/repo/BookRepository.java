7fc26a1baf5f3f05976ca6af5f14684999406d00
seonghoo1217 <seonghoo1217@naver.com>
2023-10-04 17:07:27 +0900

    feat : [Spring-Concurrency] implement Pessimistic Lock use jpa repository

--- concurrency/src/main/java/spring/concurrency/repo/BookRepository.java
+++ concurrency/src/main/java/spring/concurrency/repo/BookRepository.java
@@ -1,7 +1,14 @@
 package spring.concurrency.repo;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import spring.concurrency.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select b from Book b where b.id = :id")
+    Book findByWithPessimisticLock(@Param("id") Long id);
 }
