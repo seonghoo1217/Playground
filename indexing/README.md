bc9a78f042fdbc612331d72f2dd775653cab3094
seonghoo1217 <seonghoo1217@naver.com>
2023-11-23 20:12:15 +0900

    docs : [Indexing] 패키지 학습 목표 사항 정리

---
+++ indexing/README.md
@@ -0,0 +1,8 @@
+# Indexing
+
+인덱싱 종류에 따라 얼마나 조회 쿼리가 개선되는지 학습하는 레포지토리
+
+## Indexing Type
+- Unique
+- Single
+- Multi Column index
---
+++ indexing/src/test/java/example/indexing/IndexingApplicationTests.java
@@ -0,0 +1,13 @@
+package example.indexing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
