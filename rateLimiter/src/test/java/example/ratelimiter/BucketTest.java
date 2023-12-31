5c898a6d1b73f9407c9a43d4a41573cbff5ef4c6
Lee.t.c <lsh6451217@gmail.com>
2023-12-31 21:32:37 +0900

    test: [RateLimiter] 일정 시간 후 버킷 재생성

--- rateLimiter/src/test/java/example/ratelimiter/BucketTest.java
+++ rateLimiter/src/test/java/example/ratelimiter/BucketTest.java
@@ -13,6 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -52,4 +56,29 @@ public class BucketTest {
         assertEquals(HttpStatus.TOO_MANY_REQUESTS, responseEntity.getStatusCode());
     }
 
+    @Test
+    public void bucketConsume_retryAfterDelay() throws InterruptedException {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        // 첫 번째 요청 - 실패
+        when(mockBucket.tryConsume(1)).thenReturn(false);
+        ResponseEntity<?> responseEntity = bucketTestController.bucketAccess(request);
+        assertEquals(HttpStatus.TOO_MANY_REQUESTS, responseEntity.getStatusCode());
+
+        // 10초 후에 다시 요청
+        executorService.schedule(() -> {
+            when(mockBucket.tryConsume(1)).thenReturn(true);
+            ResponseEntity<?> response = bucketTestController.bucketAccess(request);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+        }, 10, TimeUnit.SECONDS);
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(15, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+    }
 }
